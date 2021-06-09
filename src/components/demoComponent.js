762b8daeb97317427d45448b3491ee4c840e3f64
Matthew Moniz <Matt@Fastforward.sh>
2021-06-09 11:09:46 -0400

    second commit

--- src/components/demoComponent.js
+++ src/components/demoComponent.js
@@ -1,11 +1,3 @@
-// import React, { Component } from "react";
-
-// export default class DemoComponent extends Component {
-//   render() {
-//     return <div>Card-{this.props.color}</div>;
-//   }
-// }
-
 
 import React from 'react'
 
